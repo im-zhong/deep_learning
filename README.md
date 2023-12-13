6dcfa702e53d92f6c9b2e1a103ed6db0d991dc79
zhangzhong <im.zhong@outlook.com>
2023-12-13 20:17:19 +0800

    Vision in Transformer

--- README.md
+++ README.md
@@ -8,11 +8,13 @@ My Deep Learning Codes.
 2. every single other networks, use a separate folder, and group by them by categories, such as CNN, GPT, Diffusion, etc
 3. how would that be? I don't want to use PYTHONPATH, so how to structure my code?
 4. dl/mytorch, dl/modules/CNN, LeNet, Transformers, ..., whereis my tests? it should be close to my code
-5. 因为python的路径寻找规则，当我们使用vscode直接运行当前文件的时候，python解释器会从python脚本的路径开始查找其他的模块，这当然找不到，所以我们的解决方案就是使用pytest来运行所有的程序，这样的话所有的程序都是写成单元测试的样子，这种方式非常有效！
+5.
+
+因为python的路径寻找规则，当我们使用vscode直接运行当前文件的时候，python解释器会从python脚本的路径开始查找其他的模块，这当然找不到，所以我们的解决方案就是使用pytest来运行所有的程序，这样的话所有的程序都是写成单元测试的样子，这种方式非常有效！
 
 ## TODO
 
-- [x] 还差 cnn 和一些ipynb
+- [x] 还差 scratch/quick_test.ipynb
 - [x] make sure all the tests are passes when we push our code to the github. but how? github action?
 - [ ] 等cnn也弄完之后 我们需要进行下一步的重构，这就包括更加合理的架构 更加合理的命名 更加清晰的注释等
 - [x] 单元测试花费太多时间，削减数据集的数量，进一步减少测试时间 将测试时间控制在数秒以内
@@ -22,15 +24,18 @@ My Deep Learning Codes.
 ## 我们不禁要问一个问题，在什么地方应用type hint最为合适？
 
 1. 显而易见的是函数题的签名应该做type hint
-2. 函数题内部需要做吗？比如我生命一个新的变量，调用函数返回一个变量？个人认为在可以轻松的由类型系统作出推断的场景，都不应该再使用type hint，比如调用函数的返回值，调用构造函数构造对象，而显示的声明接下来要使用的变量，且无法做初始化的时候，就应该做type hint
+2. 函数题内部需要做吗？比如我生命一个新的变量，调用函数返回一个变量？个人认为在可以轻松的由类型系统作出推断的场景，都不应该再使用type
+   hint，比如调用函数的返回值，调用构造函数构造对象，而显示的声明接下来要使用的变量，且无法做初始化的时候，就应该做type hint
 
 ## TIPS
 
 1. put all the datasets under the data/ folder
 2. 分层训练，这样就可以训练大模型了
-3. 为了在jupyter notebook里面使用我们编写的package，需要在根目录下提供一个.env文件并在内部指定PYTHONPATH=your-root-dir [https://github.com/microsoft/vscode-jupyter/issues/9436]
+3. 为了在jupyter
+   notebook里面使用我们编写的package，需要在根目录下提供一个.env文件并在内部指定PYTHONPATH=your-root-dir [https://github.com/microsoft/vscode-jupyter/issues/9436]
 4. jupyter notebook对git非常不友好 垃圾格式 不用了 不要再用了 球球了 全是错误 不会自动加载新代码
-5. start tensorboard: `conda deactivate` back to base env; then `tmux`; then `conda activate ml`; then `tensorboard --logdir=runs --bind_all`; then `ctrl+b d` to detach the tmux session
+5. start tensorboard: `conda deactivate` back to base env; then `tmux`; then `conda activate ml`;
+   then `tensorboard --logdir=runs --bind_all`; then `ctrl+b d` to detach the tmux session
 
 ## packages
 
---
+++ module/vision/vit.py
@@ -0,0 +1,148 @@
+# 2023/12/13
+# zhangzhong
+# Transformers for Vision
+
+import torch
+from torch import nn, Tensor
+
+
+class PatchEmbedding(nn.Module):
+    def __init__(self, hidden_size: int, kernel_size: int, stride: int):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.conv = nn.LazyConv2d(out_channels=hidden_size, kernel_size=kernel_size, stride=stride)
+
+    def forward(self, images: Tensor):
+        return self.conv(images).flatten(start_dim=2).transpose(1, 2)
+
+
+class PositionalEmbedding(nn.Module):
+    def __init__(self, hidden_size: int, max_len: int):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.max_len = max_len
+        self.positional_embedding = nn.Parameter(torch.randn(size=(1, max_len, hidden_size)))
+
+    def forward(self, inputs: Tensor) -> Tensor:
+        # inputs.shape = (b, s, h)
+        _, seq_size, _ = inputs.shape
+        # selected_positional_embedding = self.positional_embedding[:, :seq_size]
+        # assert selected_positional_embedding.shape == (1, seq_size, self.hidden_size)
+        return inputs + self.positional_embedding[:, :seq_size]
+
+
+class ViTMLP(nn.Module):
+    def __init__(self, hidden_size: int, output_size: int, dropout: float):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout = dropout
+        self.net = nn.Sequential(
+            nn.LazyLinear(out_features=hidden_size),
+            nn.GELU(),
+            nn.Dropout(p=dropout),
+            nn.LazyLinear(out_features=output_size),
+            nn.Dropout(p=dropout)
+        )
+
+    def forward(self, inputs: Tensor) -> Tensor:
+        return self.net(inputs)
+
+
+class ViTBlock(nn.Module):
+    def __init__(self, hidden_size: int, num_heads: int, dropout: float, mlp_hidden_size: int):
+        super().__init__()
+        self.ln1 = nn.LayerNorm(normalized_shape=hidden_size)
+        self.attention = nn.MultiheadAttention(embed_dim=hidden_size,
+                                               num_heads=num_heads,
+                                               dropout=dropout,
+                                               batch_first=True)
+        self.ln2 = nn.LayerNorm(normalized_shape=hidden_size)
+        # TODO: maybe choose another hyper-parameters
+        self.mlp = ViTMLP(hidden_size=mlp_hidden_size, output_size=hidden_size, dropout=dropout)
+
+    def forward(self, inputs: Tensor):
+        # residual connections 1: multihead attention
+        x = self.ln1(inputs)
+        attention_output, _ = self.attention(query=x, key=x, value=x)
+        inputs = inputs + attention_output
+        # residual connection 2: mlp
+        return inputs + self.mlp(self.ln2(inputs))
+
+
+class ViT(nn.Module):
+    def __init__(self, hidden_size: int, kernel_size: int, stride: int,
+                 max_len: int, dropout: float, num_blocks: int,
+                 num_heads: int, mlp_hidden_size: int):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.cls = torch.zeros(size=(1, 1, hidden_size))
+        self.patch_embedding = PatchEmbedding(hidden_size=hidden_size,
+                                              kernel_size=kernel_size,
+                                              stride=stride)
+        self.positional_embedding = PositionalEmbedding(hidden_size=hidden_size,
+                                                        max_len=max_len)
+        # TODO: there are many dropout, maybe tune them
+        self.dropout = nn.Dropout(p=dropout)
+        # ViT blocks
+        # 这样写torchinfo识别不了 还是得写成sequential？
+        blocks = [ViTBlock(hidden_size=hidden_size,
+                           num_heads=num_heads,
+                           dropout=dropout,
+                           mlp_hidden_size=mlp_hidden_size) for _ in range(num_blocks)]
+        # special <cls> token
+        self.blocks = nn.Sequential(*blocks)
+
+    def forward(self, images: Tensor):
+        # patch embedding
+        batch_size, channels, height, weight = images.shape
+        x = self.patch_embedding(images)
+        # assert x.shape == (batch_size, seq_size, self.hidden_size)
+        batch_size1, seq_size, hidden_size = x.shape
+        assert batch_size == batch_size1
+        assert hidden_size == self.hidden_size
+
+        # add cls
+        device = x.device
+        # first copy cls to the device of x
+        clss = self.cls.repeat(batch_size, 1, 1).to(device)
+
+        y = torch.concat([clss, x], dim=1)
+        assert y.shape == (batch_size, seq_size + 1, hidden_size)
+        y.requires_grad_()
+
+        # add positional embedding
+        z = self.positional_embedding(y)
+
+        # through dropout
+        embedding = self.dropout(z)
+
+        # through n vit blocks
+        # for block in self.blocks:
+        #     inputs = block(inputs)
+        outputs = self.blocks(embedding)
+
+        # get cls for outputs
+        return outputs[:, 0]
+
+
+class ViTClassifier(nn.Module):
+    def __init__(self, hidden_size: int, kernel_size: int, stride: int,
+                 max_len: int, dropout: float, num_blocks: int,
+                 num_heads: int, mlp_hidden_size: int, output_size: int):
+        super().__init__()
+        self.vit = ViT(hidden_size=hidden_size,
+                       kernel_size=kernel_size,
+                       stride=stride,
+                       max_len=max_len,
+                       dropout=dropout,
+                       num_blocks=num_blocks,
+                       num_heads=num_heads,
+                       mlp_hidden_size=mlp_hidden_size)
+        self.ln = nn.LayerNorm(hidden_size)
+        self.linear = nn.LazyLinear(out_features=output_size)
+
+    def forward(self, images: Tensor):
+        return self.linear(self.ln(self.vit(images)))
